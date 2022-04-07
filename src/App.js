8e5135b6166ce02c76f7f24239c0f4688e8a9dd7
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-04-07 15:13:10 -0500

    rolling back to react 17 (React Router v5.1 doesn't work)navbar and sidebar components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Dashboard from "./pages/dashboard/dashboard.component"
+import Dashboard from "./pages/dashboard/dashboard.component";
+import Login from "./pages/login/login.component";
+import Create from "./pages/create/create.component";
+import Signup from "./pages/signup/signup.component";
+import ProjectDetails from "./pages/project-details/project-detail.component";
+
+import Navbar from "./components/navbar/navbar.component";
+
 import "./App.css";
+import Sidebar from "./components/sidebar/sidebar.component";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Sidebar />
         <div className="container">
+          <Navbar />
           <Switch>
             <Route exact path={"/"}>
-              <Dashboard/>
+              <Dashboard />
+            </Route>
+            <Route path={"/create"}>
+              <Create />
+            </Route>
+            <Route path={"/login"}>
+              <Login />
+            </Route>
+            <Route path={"/signup"}>
+              <Signup />
+            </Route>
+            <Route path={"/projects/:id"}>
+              <ProjectDetails />
             </Route>
           </Switch>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 
-import { createRoot } from "react-dom/client";
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App tab="home" />
+      <App />
     </AuthContextProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
