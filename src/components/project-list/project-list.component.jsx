539b11dbe357a969163b8f83e186be430264a95f
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-04-12 13:25:08 -0500

    Project list component

--- src/components/project-list/project-list.component.jsx
+++ src/components/project-list/project-list.component.jsx
@@ -1,7 +1,26 @@
-import "./project-list.styles.css"
+import { Link } from "react-router-dom";
+import Avatar from "../avatar/avatar.component"
+import "./project-list.styles.css";
 
-export default function ProjectList() {
+export default function ProjectList({ projects }) {
   return (
-    <div>ProjectList</div>
-  )
+    <div className="project-list">
+      {projects.length === 0 && <p>No projects added</p>}
+      {projects.map((project) => (
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by: {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map(user=>(
+                <li key={user.id}>
+                  <Avatar photoURL={user.photoURL}/>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
--- src/pages/dashboard/dashboard.component.jsx
+++ src/pages/dashboard/dashboard.component.jsx
@@ -1,15 +1,14 @@
 import "./dashboard.stye.css";
 import { useCollection } from "../../hooks/useCollection";
+import ProjectList from "../../components/project-list/project-list.component";
 
 export default function Dashboard() {
   const { documents, error } = useCollection("projects");
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
-      {error && <p className="error">{error}</p> }
-      {documents && documents.map(doc=>(
-        <p key={doc.id}>{doc.name}</p>
-      ))}
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
     </div>
   );
 }
